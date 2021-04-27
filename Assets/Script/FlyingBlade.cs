545688ba0dd337fac226223a6a46e4e2f5c17820
eric100789 <68538369+eric100789@users.noreply.github.com>
2021-04-27 21:00:56 +0800

    add files

---
+++ Assets/Script/FlyingBlade.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlyingBlade : MonoBehaviour
+{
+    private Animator _animator;
+    [SerializeField] private GameObject flyingblade;
+    [SerializeField] private float speed = 0.5f;
+    private void Start()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.I))
+            _animator.SetTrigger("StartShout");
+        else if (Input.GetKeyUp(KeyCode.I))
+            _animator.SetTrigger("EndShout");
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            var gameObject = Instantiate(flyingblade, transform.position, Quaternion.identity);
+        }
+    }
+}
---
+++ Assets/Script/FlyingBlade_hit.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Globalization;
+
+public class FlyingBlade_hit : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private DateTime st_time;
+    [SerializeField] private float speed;
+    public Vector3 dir;
+
+
+    void Start()
+    {
+        st_time = DateTime.Now;
+    }
+
+
+    void Update()
+    {
+        if (DateTime.Now.Subtract(st_time).Seconds >= 3)
+        {
+            Destroy(this.gameObject);
+        }
+        transform.Translate(dir.normalized * speed);
+    }
+}
+
