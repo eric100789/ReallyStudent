a3bb87ee434211f6b2bf33072adf2710ca1313f5
unknown <asef18766@gmail.com>
2021-04-27 21:26:06 +0800

    add infra for your further work

---
+++ Assets/Script/BaseBullet.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace Script
+{
+    public class BaseBullet : MonoBehaviour
+    {
+        [SerializeField] protected float speed = 48763;
+        public Vector2 Dir;
+
+        protected virtual void Tick()
+        {
+            //TODO: you shall implement these :P
+            transform.Translate(Dir*speed);
+        }
+    }
+}
