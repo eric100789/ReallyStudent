a8df9b52275a1743a8462f5d7157fa9e2ba48f2c
eric100789 <68538369+eric100789@users.noreply.github.com>
2021-05-01 17:29:29 +0800

    add moreBullet

---
+++ Assets/Script/BulletFloder/KillerShooting.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace Script.BulletFloder
+{
+
+
+    public class KillerShooting : MonoBehaviour
+    {
+
+        // -------variable---------
+        [SerializeField] public List<GameObject> BulletList;
+        //--------variable---------
+
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.S))
+            {               
+                var Bullet = Instantiate(BulletList[Random.Range(0, BulletList.Count)], transform.position, Quaternion.identity);
+                Bullet.GetComponent<BaseBullet>().SetDir(Random.insideUnitCircle);
+            }
+        }
+    }
+}
---
+++ Assets/Script/BulletFloder/ZeroABullet.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+namespace Script.BulletFloder
+{
+    public class ZeroABullet : BaseBullet
+    {
+        [SerializeField] protected float SizeSpeed;
+        private DateTime _stTime;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            _stTime = DateTime.Now;
+        }
+
+        // Update is called once per frame
+        override protected void Update()
+        {
+            if (DateTime.Now.Subtract(_stTime).Seconds >= 2)
+            {
+                transform.Translate(speed, -speed, 0);
+            }
+
+            if (DateTime.Now.Subtract(_stTime).Seconds >= 3)
+            {
+                Destroy(this.gameObject);
+            }
+
+            this.gameObject.transform.localScale += new Vector3(SizeSpeed, SizeSpeed, 0);
+
+        }
+    }
+}
